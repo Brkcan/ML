97af7cac17ee14b9bc5f454519cc1365cbb5da15
Burak Can <burakcan@gmail.com>
2025-02-15 14:50:21 +0300

    lambda_callback

--- binary_accurancy.py
+++ binary_accurancy.py
@@ -6,6 +6,7 @@ Created on Thu Jan  2 00:40:21 2025
 @author: burakcan
 """
 
+import tensorflow as tf
 from tensorflow.keras.metrics import binary_accuracy
 import numpy as np
 
@@ -14,7 +15,13 @@ y = np.array([1, 0, 1, 1, 0])
 y_hat = np.array([0.90, 0.7, 0.6, 0.9, 0.6])
 
 
-result = binary_accuracy(y, y_hat)
-print(result)
+resultBinary = tf.convert_to_tensor(binary_accuracy(y, y_hat))
+
+print(resultBinary)
+
+
+
+
+
 
 
